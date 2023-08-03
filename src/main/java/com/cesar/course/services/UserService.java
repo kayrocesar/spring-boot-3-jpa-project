d0d09a979bb680755eaf9f865a55425759b5400d
kayrocesar <kayro.cesar.kc@gmail.com>
2023-08-03 18:15:11 -0300

    Criada Camada de serviço, registro de componentes

---
+++ src/main/java/com/cesar/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.cesar.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cesar.course.entities.User;
+import com.cesar.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+}
