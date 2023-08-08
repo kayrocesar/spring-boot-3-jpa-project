8689ef110efc2ca54318cfcc29744b02aab13a74
kayrocesar <kayro.cesar.kc@gmail.com>
2023-08-07 21:04:22 -0300

    Seed da entidade pedido e criação de rota para a mesma

--- src/main/java/com/cesar/course/entities/Order.java
+++ src/main/java/com/cesar/course/entities/Order.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,26 +14,27 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private Instant moment;
-	
-	@ManyToOne
-	@JoinColumn(name ="client_id")
-	private User client;
-	
-	
-	public Order() {
-		
-	}
 
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant moment;
+
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
+
+	public Order() {
+
+	}
 
 	public Order(Long id, Instant moment, User client) {
 		this.id = id;
@@ -39,42 +42,34 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public Instant getMoment() {
 		return moment;
 	}
-
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
 
-
 	public User getClient() {
 		return client;
 	}
-
 
 	public void setClient(User client) {
 		this.client = client;
 	}
 
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -87,14 +82,5 @@ public class Order implements Serializable {
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-
-	
-
-	
-	
-	
-	
 
 }
