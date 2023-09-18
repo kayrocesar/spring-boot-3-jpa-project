c882a2da42128d88613d8814d1464b74affc62a3
kayrocesar <kayro.cesar.kc@gmail.com>
2023-09-18 12:49:12 -0300

    Criada classe OrderItem com mapeamento objeto relacional

--- src/main/java/com/cesar/course/config/TestConfig.java
+++ src/main/java/com/cesar/course/config/TestConfig.java
@@ -11,10 +11,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.cesar.course.entities.Category;
 import com.cesar.course.entities.Order;
+import com.cesar.course.entities.OrderItem;
 import com.cesar.course.entities.Product;
 import com.cesar.course.entities.User;
 import com.cesar.course.entities.enums.OrderStatus;
 import com.cesar.course.repositories.CategoryRepository;
+import com.cesar.course.repositories.OrderItemRepository;
 import com.cesar.course.repositories.OrderRepository;
 import com.cesar.course.repositories.ProductRepository;
 import com.cesar.course.repositories.UserRepository;
@@ -33,6 +35,8 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 
 	@Override
@@ -70,6 +74,14 @@ public class TestConfig implements CommandLineRunner{
 		categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 	}
 }
---
+++ src/main/java/com/cesar/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,41 @@
+package com.cesar.course.entities.pk;
+
+import java.io.Serializable;
+
+import com.cesar.course.entities.Order;
+import com.cesar.course.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
+@Embeddable
+public class OrderItemPK implements Serializable{
+	private static final long serialVersionUID= 1L;
+
+	@ManyToOne
+	@JoinColumn(name= "order_id")
+	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name= "product_id")
+	private Product product;
+	
+	
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	
+	
+}
